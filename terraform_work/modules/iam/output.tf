64a3d74c17efde9f1432889a9da9b4d23b81fedd
zhang <1459318598@qq.com>
2026-02-08 23:44:37 +0800

    1

--- terraform_work/modules/iam/output.tf
+++ terraform_work/modules/iam/output.tf
@@ -1,12 +1,12 @@
-output "user_arn" {
-  value = aws_iam_user.operator.arn
-}
+# output "user_arn" {
+#   value = aws_iam_user.operator.arn
+# }
 output "role_arns" {
   value = {
-    role_1 = aws_iam_role.role_1.arn
-    role_2 = aws_iam_role.role_2.arn
-    role_3 = aws_iam_role.role_3.arn
-    role_4 = aws_iam_role.role_4.arn
+    # role_1 = aws_iam_role.role_1.arn
+    # role_2 = aws_iam_role.role_2.arn
+    # role_3 = aws_iam_role.role_3.arn
+    # role_4 = aws_iam_role.role_4.arn
     role_5 = aws_iam_role.role_5.arn
     role_6 = aws_iam_role.role_6.arn
   }
